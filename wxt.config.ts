3febbdc13deb0d734635a690a461ec02277ecdc5
不游 <71683364+mefengl@users.noreply.github.com>
2024-12-10 07:41:40 +0800

    memcode

--- wxt.config.ts
+++ wxt.config.ts
@@ -3,8 +3,16 @@ import { defineConfig } from 'wxt'
 // See https://wxt.dev/api/config.html
 export default defineConfig({
   manifest: {
-    permissions: ['activeTab'],
+    default_locale: 'en',
+    description: '__MSG_extension_description__',
+    host_permissions: [],
+    name: '__MSG_extension_name__',
+    permissions: [],
   },
-  modules: ['@wxt-dev/module-react'],
+  modules: [
+    '@wxt-dev/module-react',
+    '@wxt-dev/auto-icons',
+    '@wxt-dev/i18n/module',
+  ],
   srcDir: 'src',
 })
