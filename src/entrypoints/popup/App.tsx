5b77cc0418fd9290fd90cbbbe84eaf010419109f
不游 <71683364+mefengl@users.noreply.github.com>
2024-12-11 16:48:24 +0800

    update github issues link

--- src/entrypoints/popup/App.tsx
+++ src/entrypoints/popup/App.tsx
@@ -210,7 +210,7 @@ function Popup() {
 
       <div className="flex justify-between border-t px-3 py-2">
         <Button
-          onClick={() => window.open('https://github.com/mefengl/memcode/issues/new', '_blank')}
+          onClick={() => window.open('https://github.com/mefengl/devdocs-finder/issues/new', '_blank')}
           size="sm"
           variant="outline"
         >
